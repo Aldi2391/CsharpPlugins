4b5f27351e3571558185e3ad4e19cdc6c1195f4b
Alekseev Dmitrii <alex.ds.91@yandex.ru>
2024-10-28 15:58:31 +0300

    Создание основных окон для работы приложения

--- BIMEXT/SharedModelUnloader/ViewModels/MainWindowViewModel.cs
+++ BIMEXT/SharedModelUnloader/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,12 @@ namespace SharedModelUnloader.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        public string UserName { get; }
+        
+
+        public MainWindowViewModel()
+        {
+            UserName = Environment.UserName ?? throw new Exception("Ошибка при получении имени пользователя");
+        }
     }
 }
